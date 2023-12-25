61cdae410208b61907cec1dfbf4f06d2ae92f8f1
andrew-jindev <andrew.jindev@kakaohealthcare.com>
2023-12-25 20:44:37 +0900

    feat: 컨트롤러, 리포지토리 작업 - map 을 활용하여 O(1) 으로 최적화

--- src/main/java/com/slicequeue/inflearn/repository/order/query/OrderQueryRepository.java
+++ src/main/java/com/slicequeue/inflearn/repository/order/query/OrderQueryRepository.java
@@ -1,5 +1,7 @@
 package com.slicequeue.inflearn.repository.order.query;
 
+import java.util.Map;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
@@ -13,11 +15,31 @@ public class OrderQueryRepository {
     private final EntityManager em;
 
     public List<OrderQueryDto> findOrderQueryDtos() {
-        return em.createQuery(
-                "select new com.slicequeue.inflearn.repository.order.query.OrderQueryDto(o.id, m.name, o.orderDate, o.status, d.address) from Order o" +
-                        " join o.member m" +
-                        " join o.delivery d",
-                OrderQueryDto.class).getResultList();
+      List<OrderQueryDto> result = findOrders();
+      result.forEach(o -> {
+        List<OrderItemQueryDto> orderItems = findOrderItems(o.getOrderId());
+        o.setOrderItems(orderItems);
+      });
+      return result;
+    }
+
+    public List<OrderQueryDto> findAllByDto_optimization() {
+      List<OrderQueryDto> result = findOrders();
+      List<Long> orderIds = result.stream().map(OrderQueryDto::getOrderId).toList();
+
+      // in 절과 map 화 시켜서 1번만 불리도록 함
+      List<OrderItemQueryDto> orderItems = em.createQuery(
+              "select new com.slicequeue.inflearn.repository.order.query.OrderItemQueryDto(oi.order.id, i.name, oi.orderPrice, oi.count)"
+                  +
+                  " from OrderItem oi" +
+                  " join oi.item i" +
+                  " where oi.order.id in :orderIds", OrderItemQueryDto.class)
+          .setParameter("orderIds", orderIds).getResultList();
+      Map<Long, List<OrderItemQueryDto>> orderItemMap = orderItems.stream()
+          .collect(Collectors.groupingBy(OrderItemQueryDto::getOrderId));
+      // map 을 활용하여 O(1) 으로 최적화
+      result.forEach(o -> o.setOrderItems(orderItemMap.get(o.getOrderId())));
+      return result;
     }
 
     public List<OrderItemQueryDto> findOrderItems(Long orderId) {
@@ -28,4 +50,11 @@ public class OrderQueryRepository {
                 .setParameter("orderId", orderId)
                 .getResultList();
     }
+
+  private List<OrderQueryDto> findOrders() {
+    return em.createQuery("select new com.slicequeue.inflearn.repository.order.query.OrderQueryDto(o.id, m.name, o.orderDate, o.status, d.address) from Order o" +
+        " join o.member m" +
+        " join o.delivery d", OrderQueryDto.class
+    ).getResultList();
+  }
 }
